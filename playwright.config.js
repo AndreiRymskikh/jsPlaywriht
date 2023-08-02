e874eb1a863a0dbeb48c4b272df03c4fe8ffea12
Andrei.Rymskikh <v540509@mwiah.co.uk>
2023-08-02 13:28:40 +0100

    Login test was added

--- playwright.config.js
+++ playwright.config.js
@@ -35,19 +35,8 @@ module.exports = defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
-
+      use: { ...devices['Desktop Chrome'] }
+    }
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
--- testsF/SimpleUITests.spec.js
+++ testsF/SimpleUITests.spec.js
@@ -7,3 +7,21 @@ test('Open web site and verify Url and Title', async ({ page }) => {
   await expect(page).toHaveTitle(/LoginPage Practise | Rahul Shetty Academy/);
   await expect(page).toHaveURL(/.*loginpagePractise/);
 });
+
+test('Login web site and check that User authorised successfully', async ({ page }) => {
+  const passwordField = page.locator('#password');
+  const usernameField = page.locator('#username');
+  const signInBtn = page.locator('#signInBtn');
+  const card = page.locator('.card-body a');
+  
+  await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+
+  await expect(page).toHaveTitle(/LoginPage Practise | Rahul Shetty Academy/);
+  await expect(page).toHaveURL(/.*loginpagePractise/);
+
+  await usernameField.type("rahulshettyacademy");
+  await passwordField.type("learning");
+  await signInBtn.click();
+
+  console.log(card.nth(0).textContent);
+});
