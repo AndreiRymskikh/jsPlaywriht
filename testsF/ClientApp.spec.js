1dbcfeb4d2ffb8b9b7957ba83903c961d9d9a47a
Andrei.Rymskikh <v540509@mwiah.co.uk>
2023-08-16 20:43:37 +0100

    Add zara coat to card

--- testsF/ClientApp.spec.js
+++ testsF/ClientApp.spec.js
@@ -5,6 +5,7 @@ test('Open web site and verify Url and Title', async ({ page }) => {
   const userEmailField = page.locator("#userEmail");
   const passwordField = page.locator("#userPassword");
   const loginBtn = page.locator("#login");
+  const products = page.locator(".card-body");
 
   await page.goto('https://rahulshettyacademy.com/client/');
 
@@ -15,4 +16,15 @@ test('Open web site and verify Url and Title', async ({ page }) => {
 
   const titles = await page.locator(".card-body b").allTextContents();
   console.log(titles);
+
+  const productName = "Zara Coat 4";
+  const prodQty = await products.count();
+  for(let i = 0; i < prodQty; i++)
+  {
+    if( await products.nth(i).locator("b").textContent() === productName)
+    {
+      await products.nth(i).locator("text = Add To Card").click();
+      break;
+    }
+  }
 });
