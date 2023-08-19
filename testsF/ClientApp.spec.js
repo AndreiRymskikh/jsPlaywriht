39247957d17e4a4217458cd8bab0c9d601ca86f6
Andrei.Rymskikh <v540509@mwiah.co.uk>
2023-08-19 13:24:56 +0100

    Check that order was added successfully

--- testsF/ClientApp.spec.js
+++ testsF/ClientApp.spec.js
@@ -6,10 +6,11 @@ test('Check product in the cart', async ({ page }) => {
   const passwordField = page.locator("#userPassword");
   const loginBtn = page.locator("#login");
   const products = page.locator(".card-body");
+  const email = "anshika@gmail.com";
 
   await page.goto('https://rahulshettyacademy.com/client/');
 
-  await userEmailField.fill("anshika@gmail.com");
+  await userEmailField.fill(email);
   await passwordField.type("Iamking@000");
   await loginBtn.click();
   await page.waitForLoadState('networkidle');
@@ -28,11 +29,33 @@ test('Check product in the cart', async ({ page }) => {
     }
   }
   await page.locator("[routerlink*= 'cart']").click();
-  await page.pause();
   await page.locator("div li").first().waitFor();
 
   const isElemVisible = page.locator("h3:has-text('zara coat 3')").isVisible();
 
   expect(isElemVisible).toBeTruthy();
 
+  await page.locator("text=Checkout").click();
+  await page.locator("[placeholder*='Country']").type("ind", {delay: 100});
+
+  const options = page.locator(".ta-results");
+  await options.waitFor();
+  const optionsCount = await options.locator("button").count();
+
+  for(let i = 0; i < optionsCount; ++i){
+    const text = await options.locator("button").nth(1).textContent();
+
+    if(text === " India")
+    {
+      await options.locator("button").nth(1).click();
+      break;
+    }
+  }
+
+  expect(page.locator(".user__name [type='text']").first()).toHaveText(email);
+  await page.pause();
+  await page.locator(".action__submit").click();
+
+  await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ")
+//  await page.pause();
 });
